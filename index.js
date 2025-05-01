0a244eae27194420e3833c8656cd5f01c4bdfd41
Dilusha2009 <dilushachamod29@gmail.com>
2025-05-01 08:28:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
 
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-  <title>LOKU NIMA</title>
+  <title>DILLA WEB IMFO</title>
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
 
